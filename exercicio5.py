11c4f3599dbc7da2907748138ac0c2e31eec9427
beatriz-rios <beatriz.rios6@aluno.senai.br>
2025-11-17 17:28:35 -0300

    ...

--- exercicio5.py
+++ exercicio5.py
@@ -4,8 +4,7 @@ valor = quant * camisa
 
 if quant <= 5:
  print(f" O preço da camisa é: {valor * 0.97:.2f}")
+elif  quant <= 10:
+    print (f"O preço da camisa é: {valor * 0.95:.2f}")    
 else:
-   if  quant <= 10:
-    print (f"O preço da camisa é: {valor * 0.95:.2f}")
-   else:
     (f"O preço da camisa é: {camisa * 0.93:.2f}")
