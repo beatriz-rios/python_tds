1704ba0d7d0e3024c53126a7312055b23fc67eaa
beatriz-rios <beatriz.rios6@aluno.senai.br>
2025-11-17 16:58:12 -0300

    ...

---
+++ exercicio5.py
@@ -0,0 +1,11 @@
+camisa = 12.50
+quant = int(input("Quantas camisas você vai levar?"))
+valor = quant * camisa
+
+if quant <= 5:
+ print(f" O preço da camisa é: {valor * 0.97:.2f}")
+else:
+   if  quant <= 10:
+    print (f"O preço da camisa é: {valor * 0.95:.2f}")
+   else:
+    (f"O preço da camisa é: {camisa * 0.93:.2f}")
